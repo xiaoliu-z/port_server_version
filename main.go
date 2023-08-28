fdecc9f50ec65d01c618fd65fe332c1fe342941c
xiaoliu <71584404+xiaoliu-z@users.noreply.github.com>
2023-08-28 21:49:32 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,16 +1,1 @@
-package main
-
-import (
-	"fmt"
-	"github.com/xiaoliu-z/port_server_version/scan_port"
-	"strconv"
-)
-
-func main() {
-	banner, os, version, vendorProductName, server := scan_port.GetBannerServer("101.42.251.47", 22)
-	fmt.Println(strconv.Quote(string(banner)))
-	fmt.Println(os)
-	fmt.Println(version)
-	fmt.Println(vendorProductName)
-	fmt.Println(server)
-}
+https://mp.weixin.qq.com/s/hq-z2HBGz3nehnCVg_H-RQ
