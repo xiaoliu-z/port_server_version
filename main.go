3479ff5b9737548cb2c56f1fc9f77c87d276a568
xiaoliu <71584404+xiaoliu-z@users.noreply.github.com>
2023-08-30 21:39:37 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,4 +1,13 @@
 wget https://dl.google.com/go/go1.21.0.linux-amd64.tar.gz
 tar -zxvf go1.21.0.linux-amd64.tar.gz -C /usr/local
+
+/etc/profile
+
 export GOROOT=/usr/local/go
 export PATH=$PATH:$GOROOT/bin
+
+nano ~/.profile
+source ~/.profile
+
+nano ~/.bashrc
+source ~/.bashrc
