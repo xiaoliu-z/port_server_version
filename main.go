8bff8481e40c82171b752ed6941ff1f280b04f7d
xiaoliu <71584404+xiaoliu-z@users.noreply.github.com>
2023-08-30 21:04:18 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,1 +1,4 @@
-https://www.viewintech.com/detail?id=19
+wget https://dl.google.com/go/go1.21.0.linux-amd64.tar.gz
+tar -zxvf go1.21.0.linux-amd64.tar.gz -C /usr/local
+export GOROOT=/usr/local/go
+export PATH=$PATH:$GOROOT/bin
